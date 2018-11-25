786d787c7a6afafbe524d4796c343f40350775f2
Jéssica Magalhães Mourão <38639511+jemalicisou@users.noreply.github.com>
2018-11-25 12:33:16 -0200

    Declaração de variáveis e operações matemáticas

---
+++ calculo.py
@@ -0,0 +1,21 @@
+""" Cálculos básicos com Python"""
+"""Faça um algoritmo para resolver a equação z = x + y - 4,
+ armazenando o resultado na variável z.
+ Considere que os valores iniciais de x e y são, respectivamente, 10 e 5.
+Depois disso exiba o resultado."""
+
+x = 10
+y = 5
+z = x + y -4
+
+print(z)
+""""Faça um algoritmo para resolver a equação u = a * a - b + c,armazenando
+o resultado na variável z. Considere que os valores iniciais de a, b e c são,
+respecitvamente, 4, 6.5 e 10.5. Depois disso exiba o resultado."""
+
+a = 4
+b = 6.5
+c = (10.5)
+
+u= a * a -b + c
+print (u)
